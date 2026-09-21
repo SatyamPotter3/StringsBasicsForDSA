ede890782569a5ff23f43d0beb4bded36e813782
Satyam tripath <satyamtripathi1357@gmail.com>
2026-09-21 16:51:24 +0530

    New Problem

---
+++ src/countWord.java
@@ -0,0 +1,6 @@
+public class countWord {
+    public static void main(String[] args){
+        String s1="she is my Serena";
+      String words[]=s1.split(" ");
+        System.out.println(words.length);
+}}
